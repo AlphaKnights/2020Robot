04810df702e9642c5280f0e3b40a4874d8b54597
Ryan O'Donnell <rpodonnell2012@gmail.com>
2020-02-22 16:13:08 -0800

    new constants and stuff

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,6 +7,8 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants.  This class should not public final int Jacksgirlfirends = 0 be used for any other purpose.  All constants should be
@@ -57,5 +59,12 @@ public final class Constants {
     
     public final class turretSubsystemConstants {
         public static final int turretRotatorTalonID = 0;
+    }
+
+    public final class robotLiftSubsystem{
+        public static final int Talon1 = 0;
+        public static final int Talon2 = 1;
+        public static final int Talon3 = 2;
+        
     }
 }
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -12,11 +12,11 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
-//import com.revrobotics.CANSparkMax;
-//import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+// import com.revrobotics.CANSparkMax;
+// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class IntakeSubsystem extends SubsystemBase {
- // CANSparkMax actuatingMotor = new CANSparkMax(IntakeConstants.actuateMotorID, MotorType.kBrushless);
+  // CANSparkMax actuatingMotor = new CANSparkMax(IntakeConstants.actuateMotorID, MotorType.kBrushless);
   WPI_TalonSRX IntakingMotor = new WPI_TalonSRX(IntakeConstants.intakeMotorID);
   /**
    * Creates a new IntakeSubsystem.
