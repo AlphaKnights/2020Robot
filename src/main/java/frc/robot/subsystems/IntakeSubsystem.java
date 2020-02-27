9e5abe4f032b5f3ecd9879732cfa55b59e70b27d
Emma Wong <pinkdolphin1427@gmail.com>
2020-02-26 18:13:10 -0800

    Fixed github files

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,40 +1,64 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
-// import com.revrobotics.CANSparkMax;
-// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import frc.robot.Constants.IntakeConstants.*;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class IntakeSubsystem extends SubsystemBase {
-  // CANSparkMax actuatingMotor = new CANSparkMax(IntakeConstants.actuateMotorID, MotorType.kBrushless);
+  CANSparkMax actuatingMotor = new CANSparkMax(IntakeConstants.actuateMotorID, MotorType.kBrushless);
   WPI_TalonSRX IntakingMotor = new WPI_TalonSRX(IntakeConstants.intakeMotorID);
+  DigitalInput topMagneticLimitSwitch = new DigitalInput(IntakeConstants.actuatingSensorTopDIOChannel);
+  DigitalInput bottomMagneticLimitSwitch = new DigitalInput(IntakeConstants.actuatingSensorBottomDIOChannel);
+  private int motorDirection = 1;
+  private int intakeState = 0;
+  private boolean test = true;//test for github commit
   /**
    * Creates a new IntakeSubsystem.
    */
   public IntakeSubsystem() {
-      topMagneticLimitSwitch = new DigitalInput();
-      bottomMagneticLimitSwitch = new DigitalInput();
       
     
   }
 
+  void actuateIntake(){
+    if(motorDirection == 1){
+      if(bottomMagneticLimitSwitch.get()){
+        motorDirection = motorDirection*-1;
+      }
+      else{
+        actuatingMotor.set(.2*motorDirection);
+      }
+    }
+    if(motorDirection == -1){
+      if(topMagneticLimitSwitch.get()){
+        motorDirection = motorDirection*-1;
+      }
+      else{
+        actuatingMotor.set(.2*motorDirection);
+      }
+    }
+  }
 
+  void intakeChangeState(){
+    //if()
+  }
+  
   void Extend(){
 
   }
 
   @Override
   public void periodic() {
+    if(intakeState == 1){
+      //IntakingMotor.set(1,1);
+    }
     // This method will be called once per scheduler run
   }
 }
